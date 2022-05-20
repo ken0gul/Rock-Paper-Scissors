1520823f07fc70c0a1138e67d2a39f6e5c07e399
ken0gul <ken0gul@gmail.com>
2022-05-19 23:51:57 -0400

    Check if input is valid

--- script.js
+++ script.js
@@ -5,22 +5,29 @@ let pc = 0;
 
 
 let  guess = function() {
-    return prompt('Enter your guess here');
+    let guess = prompt('Enter your guess here');
+    return guess;
 };
 
 
 function game() {
     let array = [];
     
-    
         for (let i=0; i < 5; i++) {
+            
             let result = playRound();
             array.push(result);
             console.log(result);
             console.log(user,pc);
     }
 
-   console.log( user > pc ?  `USER WON` : `PC WON`);
+   if(user > pc) {
+       console.log("USER WON!")
+   } else if( pc > user) {
+       console.log("PC WON!")
+   } else {
+       console.log("NOBODY WINS")
+   }
     
     
 }
@@ -32,19 +39,24 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = guess();
+    console.log(typeof playerSelection)
     computerSelection = computerPlay();
-    if (playerSelection === computerSelection) {
-        return `No one wins!`
-    } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
-        user++;
-        return `You win! Rock beats Scissor!`
-    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
-        pc++;
-        return `You lose! Rock beats Paper`
-    } 
+    if(!playerSelection || playerSelection.trim().length === 0 ) return alert('Input is invalid');   
+
+        
+        if (playerSelection === computerSelection) {
+            return `No one wins!`
+        } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+            user++;
+            return `You win! Rock beats Scissor!`
+        } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+            pc++;
+            return `You lose! Rock beats Paper`
+        } 
         return `You win! Scissor beats Paper!`
+    } 
+        
 
 
-}
 
 game();
