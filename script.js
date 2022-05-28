85e9b6a9374e3aa157a7d452f399e89f647c5da4
ken0gul <ken0gul@gmail.com>
2022-05-28 15:47:47 -0400

    Restructure the whole app to add UI

--- script.js
+++ script.js
@@ -1,64 +1,122 @@
-"use strict"
-
-// create user and pc variables to keep track of scores
+// Array to store user input from clicking buttons
+let scoresArray =[];
+let userInput;
 let user = 0;
 let pc = 0;
+let isGameOver = false;
 
-// get user input using prompt
-let  guess = function() {
-    let guess = prompt('Enter your guess here');
-    return guess;
-};
+// Creating buttons and displayin rounds
 
-
-function game() {
-    // declare an array variable to store each round
-    let array = [];
-    
-        // use for loop to push each round to the array until 5 round is over
-        for (let i=0; i < 5; i++) {
-            let result = playRound();
-            array.push(result);
-    }
-    // Check who wins and log it accordingly
-   if(user > pc) {
-       console.log("USER WON!")
-   } else if( pc > user) {
-       console.log("PC WON!")
-   } else {
-       console.log("NOBODY WINS")
-   }
-    
-    
-}
-
-// Create random computer input
-function computerPlay() {
-    const data = ["rock", "paper", "scissor"];
-    let random = Math.floor(Math.random() * 3);
-    return data[random];
-}
-
-function playRound(playerSelection, computerSelection) {
-    // set parameters to invoked functions below to get data from them
-    playerSelection = guess();
-    computerSelection = computerPlay();
-
-    // check if input is empty
-    if(!playerSelection || playerSelection.trim().length === 0 ) return alert('Input is invalid');   
-
+let buttons = `
+    <div class="container">
+        <button>rock</button>
+        <button>paper</button>
+        <button>scissor</button>
         
-        if (playerSelection === computerSelection) {
-            return `No one wins!`
-        } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
-            user++;
-            return `You win! Rock beats Scissor!`
-        } else if(playerSelection === 'paper' && computerSelection === 'rock') {
-            pc++;
-            return `You lose! Rock beats Paper`
-        } 
-        return `You win! Scissor beats Paper!`
-    } 
+        </div>
+        <p class="log"></p>
 
-    // invoke the game function to start the game
-game();
+        `
+
+// Creating Score Table
+let scores = `
+        <div class="scores">
+            <p class="score score__user">USER : 0</p>
+            <p class="score score__computer">Computer : 0</p>
+            </div>
+
+`// Creating WINNER Element
+let winner = document.createElement('p');
+winner.classList.add('winner');
+document.body.appendChild(winner);
+
+
+    document.body.insertAdjacentHTML("afterbegin", scores)
+    // Insert them to body
+    document.body.insertAdjacentHTML("beforeend", buttons);
+
+    // Create random computer input
+    function computerPlay() {
+        const data = ["rock", "paper", "scissor"];
+        let random = Math.floor(Math.random() * 3);
+        return data[random];
+    }
+
+    // Selecting elements
+    const para = document.querySelector('.log');
+    const scoreUser = document.querySelector('.score__user')
+    const scoreComputer = document.querySelector('.score__computer')
+    
+    
+    // Selecting buttons
+    document.querySelectorAll('button').forEach(btn => {
+        btn.addEventListener('click', e => {
+            userInput = e.target.textContent;
+            computerInput = computerPlay();
+            // When no one wins
+            if(userInput === computerInput) para.textContent = 'Nobody WIN!'
+            // When user Win!
+            if(userInput === 'scissor' && computerInput === 'paper') {
+                para.textContent =`User: ${userInput} - Computer: ${computerInput} User WINS!`
+                user++;
+                scoreUser.textContent = `USER : ${user}`
+                
+            }
+            if(userInput === 'rock' && computerInput === 'scissor') {
+                para.textContent =`User: ${userInput} - Computer: ${computerInput} User WINS!` 
+                user++;
+                scoreUser.textContent = `User : ${user}`
+            }
+            if(userInput === 'paper' && computerInput === 'rock') {
+                para.textContent =`User: ${userInput} - Computer: ${computerInput} User WINS!` 
+                user++;
+                scoreUser.textContent = `User : ${user}`
+            }
+            // When computer Win!
+            if(userInput === 'rock' && computerInput === 'paper') { 
+                para.textContent =`User: ${userInput} - Computer: ${computerInput} Computer WINS!`
+                pc++;
+                scoreComputer.textContent = `Computer : ${pc}`
+            }
+            if(userInput === 'paper' && computerInput === 'scissor') { 
+                para.textContent =`User: ${userInput} - Computer: ${computerInput} Computer WINS!`
+                pc++;
+                scoreComputer.textContent = `Computer : ${pc}`
+            }
+            if(userInput === 'scissor' && computerInput === 'rock') { 
+                para.textContent =`User: ${userInput} - Computer: ${computerInput} Computer WINS!`
+                pc++;
+                scoreComputer.textContent = `Computer : ${pc}`
+            }
+            if(user + pc === 5) {
+                isGameOver = true;
+                if (user > pc)   {
+                   winner.textContent = `USER WIN!`
+                    
+                }
+                else  { 
+                    winner.textContent = `COMPUTER WIN!`
+                    
+                }
+                
+            }
+
+            if (isGameOver) {
+                document.querySelector('.container').style.opacity = '0.2'
+                document.querySelector('.container').style.display = 'none';
+                winner.style.textAlign = 'center'
+                winner.style.fontSize = '50px'
+
+            }
+
+          
+            
+        })
+       
+        
+    });
+   
+    
+
+    
+    
